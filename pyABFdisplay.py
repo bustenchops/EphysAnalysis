f93c16fc6f3c83a60e93b188f442caf9ce8a84bd
bustenchops <mmmm_donut@msn.com>
2021-02-20 00:22:42 -0500

    pyABFdisplay is working for quick exporting of traces

--- pyABFdisplay.py
+++ pyABFdisplay.py
@@ -2,9 +2,12 @@ import pyabf
 import re
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+
 
 def showoptions():
-    selection = input('1. Show file info in browser: 2. Plot all sweeps: 3: Plot select sweeps')
+    selection = input('1. Show file info: 2. Plot all sweeps: 3. Plot select sweeps: 4. Plot all sweeps in 3D: 5. Take all in 3d and save')
     if selection == '1':
         showheader()
         showoptions()
@@ -15,36 +18,112 @@ def showoptions():
             if selection == '3':
                 plotselected()
             else:
-                print('invalid selection try again')
-                showoptions()
+                if selection == '4':
+                    threedee()
+                else:
+                    if selection == '5':
+                        sumandsave()
+                    else:
+                        print('invalid selection try again')
+                        showoptions()
+
 
 def showheader():
     print(abf)
+
 
 def plotall():
     fig = plt.figure(figsize=(10, 8))
     fig.suptitle('All Sweeps in File')
     abf.setSweep(sweepNumber=1, channel=0)
-    ax = fig.add_subplot(12, 1, (1, 5))
-    ax.set_xlabel(abf.sweepLabelX)
+    ax = fig.add_subplot(12, 1, (1, 8))
+    # ax.set_xlabel(abf.sweepLabelX)
     ax.set_ylabel(abf.sweepLabelY)
     abf.setSweep(sweepNumber=1, channel=1)
-    bx = fig.add_subplot(12, 1, (6, 10))
-    bx.set_xlabel(abf.sweepLabelX)
+    bx = fig.add_subplot(12, 1, (9, 10))
+    # bx.set_xlabel(abf.sweepLabelX)
     bx.set_ylabel(abf.sweepLabelY)
     abf.setSweep(sweepNumber=1, channel=2)
-    cx = fig.add_subplot(12, 1, (11,12))
+    cx = fig.add_subplot(12, 1, (11, 12))
     cx.set_xlabel(abf.sweepLabelX)
     cx.set_ylabel(abf.sweepLabelY)
-    for sweepNumber in abf.sweepList:
-        abf.setSweep(sweepNumber, channel=0)
+    for sweepnum in abf.sweepList:
+        abf.setSweep(sweepnum, channel=0)
         ax.plot(abf.sweepX, abf.sweepY, alpha=.5)
-        abf.setSweep(sweepNumber, channel=1)
-        bx.plot(abf.sweepX, abf.sweepY, alpha=.5)
-        abf.setSweep(sweepNumber, channel=2)
-        cx.plot(abf.sweepX, abf.sweepY, alpha=.5)
-    plt.legend()
+    abf.setSweep(0, channel=1)
+    bx.plot(abf.sweepX, abf.sweepC, alpha=.5)
+    abf.setSweep(0, channel=2)
+    cx.plot(abf.sweepX, abf.sweepY, alpha=.5)
     plt.show()
+
+
+def plotselected():
+    numsweep = int(input('How many sweeps to plot?'))
+    sweeparray = np.zeros(numsweep, dtype=int)
+    for x in range(numsweep):
+        sweeparray[x] = int(input('Sweep no:'))
+    fig = plt.figure(figsize=(10, 8))
+    fig.suptitle('Selected Sweeps from File')
+    abf.setSweep(sweepNumber=1, channel=0)
+    ax = fig.add_subplot(12, 1, (1, 8))
+    # ax.set_xlabel(abf.sweepLabelX)
+    ax.set_ylabel(abf.sweepLabelY)
+    abf.setSweep(sweepNumber=1, channel=1)
+    bx = fig.add_subplot(12, 1, (9, 10))
+    # bx.set_xlabel(abf.sweepLabelX)
+    bx.set_ylabel(abf.sweepLabelY)
+    abf.setSweep(sweepNumber=1, channel=2)
+    cx = fig.add_subplot(12, 1, (11, 12))
+    cx.set_xlabel(abf.sweepLabelX)
+    cx.set_ylabel(abf.sweepLabelY)
+    for sweepnum in sweeparray:
+        abf.setSweep(sweepnum, channel=0)
+        ax.plot(abf.sweepX, abf.sweepY, alpha=.5, label="Sweep %d" % sweepnum)
+    abf.setSweep(0, channel=1)
+    bx.plot(abf.sweepX, abf.sweepC, alpha=.5)
+    abf.setSweep(0, channel=2)
+    cx.plot(abf.sweepX, abf.sweepY, alpha=.5)
+    ax.legend()
+    plt.show()
+
+
+def threedee():
+    fig = plt.figure(figsize=(10, 5))
+    fig.suptitle('All Sweeps in File')
+    abf.setSweep(sweepNumber=1, channel=0)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.axis('Off')
+    for sweepnum in abf.sweepList:
+        abf.setSweep(sweepnum, channel=0)
+        i1, i2 = 0, int(abf.dataRate * 1.5)
+        dataX = abf.sweepX[i1:i2] + .025 * sweepnum
+        dataY = abf.sweepY[i1:i2] + 10 * sweepnum
+        ax.plot(dataX, dataY, alpha=.5)
+    plt.show()
+
+
+def sumandsave():
+    for abflist in abffiles:
+        foundfile = ''.join(abflist)
+        print(foundfile)
+        fullfilepath = os.path.join(fileloc, foundfile)
+        abf = pyabf.ABF(fullfilepath)
+        filename = Path(fullfilepath)
+        filename_replace = filename.with_suffix('.jpg')
+        print(filename)
+        fig = plt.figure(figsize=(10, 5))
+        fig.suptitle(abflist)
+        abf.setSweep(sweepNumber=1, channel=0)
+        ax = fig.add_subplot(1, 1, 1)
+        ax.axis('Off')
+        for sweepnum in abf.sweepList:
+            abf.setSweep(sweepnum, channel=0)
+            i1, i2 = 0, int(abf.dataRate * 1.5)
+            dataX = abf.sweepX[i1:i2] + .025 * sweepnum
+            dataY = abf.sweepY[i1:i2] + 10 * sweepnum
+            ax.plot(dataX, dataY, alpha=.5)
+        fig.savefig(filename_replace)
+
 
 print('IMPORTING DIRECTORIES')
 homefolder = input('Are the files in the folder /home/pi/ephys/ (y or n)?')
@@ -57,7 +136,7 @@ experdate = input('Enter the experiment date in yyyy-mm-dd format ')
 print('Subfolders within home folder:')
 print(os.listdir(homefolder))
 fileloc = os.path.join(homefolder, experdate)
-if (os.path.exists(fileloc)):
+if os.path.exists(fileloc):
     print('Files within experiment date folder')
     print(os.listdir(os.path.join(homefolder, experdate)))
     fileloclist = os.listdir(os.path.join(homefolder, experdate))
@@ -73,6 +152,7 @@ abffiletoimport = input('Type in file number (ex:0012)')
 print('Searching files:')
 abffiletoimport = [file for file in abffiles if re.search(abffiletoimport, file)]
 print('Found: ', abffiletoimport)
+# converts list to string
 foundfile = ''.join(abffiletoimport)
 print(foundfile)
 fullfilepath = os.path.join(fileloc, foundfile)
--- pyabftesting.py
+++ pyabftesting.py
@@ -1,13 +1,23 @@
 import pyabf
 import matplotlib.pyplot as plt
+import numpy as np
 
 abf = pyabf.ABF("/home/pi/ephys/2021-02-16/2021_02_16_0009.abf")
-# print(abf.headerText) # display header information in the console
-# abf.headerLaunch() # display header information in a web browser
+# abf = pyabf.ABF("/home/2021_02_10_0003.abf")
+# # print(abf.headerText) # display header information in the console
+# # abf.headerLaunch() # display header information in a web browser
+#
+#
+# arf = 0
+# abf.setSweep(sweepNumber=arf, channel=1)
+# plt.figure(figsize=(8, 5))
+# plt.plot(abf.sweepX, abf.sweepY)
+# plt.show()
 
-
-arf = 10
-abf.setSweep(sweepNumber=arf, channel=0)
-plt.figure(figsize=(8, 5))
-plt.plot(abf.sweepX, abf.sweepY)
-plt.show()
+# numsweep = int(input('How many sweeps to plot?'))
+# sweeparray = np.zeros(numsweep, dtype=int)
+# print(sweeparray)
+# for x in range(numsweep):
+#     sweeparray[x] = int(input('Sweep no:'))
+# print(sweeparray)
+print(abf.sweepPointCount)
