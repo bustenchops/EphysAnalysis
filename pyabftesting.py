b15a42788faa2bb996b044e68bce43b3962bfe87
bustenchops <mmmm_donut@msn.com>
2021-02-21 00:08:45 -0500

    in the pyabftesteing I played with code to pull out relevant data from the headerText and get it into numerical format...good for getting the info from the file.

--- pyabftesting.py
+++ pyabftesting.py
@@ -1,17 +1,34 @@
 import pyabf
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 
 abf = pyabf.ABF("/home/pi/ephys/2021-02-16/2021_02_16_0009.abf")
 # abf = pyabf.ABF("/home/2021_02_10_0003.abf")
-# # print(abf.headerText) # display header information in the console
-# # abf.headerLaunch() # display header information in a web browser
+# print(abf.headerText) # display header information in the console
+# abf.headerLaunch() # display header information in a web browser
+si = abf.headerText
+print(si)
+finditall = re.findall(r"dataSecPerPoint = \S+", si)
+sss = finditall[0]
+print(sss)
+# print(finditall)
+split = re.split(r'\s',sss)
+sampleinterval = split[2]
+npversion = float(sampleinterval)
+
+print(npversion)
+kirk = npversion * 10
+print(kirk)
 #
 #
-# arf = 0
+# xvariablesweep = np.array(abf.sweepX)
+# yvariablesweep = np.array(abf.sweepY)
+# arf = 2
 # abf.setSweep(sweepNumber=arf, channel=1)
 # plt.figure(figsize=(8, 5))
-# plt.plot(abf.sweepX, abf.sweepY)
+# plt.plot(xvariablesweep, yvariablesweep)
+# # plt.plot(abf.sweepX, abf.sweepD(2))
 # plt.show()
 
 # numsweep = int(input('How many sweeps to plot?'))
@@ -20,4 +37,9 @@ abf = pyabf.ABF("/home/pi/ephys/2021-02-16/2021_02_16_0009.abf")
 # for x in range(numsweep):
 #     sweeparray[x] = int(input('Sweep no:'))
 # print(sweeparray)
-print(abf.sweepPointCount)
+# print(abf.sweepPointCount)
+#
+# print (dir(pyabf.ABF))
+
+
+
--- trestingbed.py
+++ trestingbed.py
@@ -4,4 +4,34 @@ import re
 import os
 
 homefolder = '/home/pi/ephys/2021-02-16/2021_02_16_0009.abf'
-abf = AxonIO(filename=homefolder)
+# abf = AxonIO(filename=homefolder)
+reader = AxonIO(filename=homefolder)
+nblocks = reader.block_count()
+si = reader.get_signal_sampling_rate()
+samplesize = reader.get_signal_size(block_index=0, seg_index=0)
+tstart = reader.segment_t_start(block_index=0, seg_index=0)
+tstop = reader.segment_t_stop(block_index=0, seg_index=0)
+header = reader.header
+nchannels = reader.signal_channels_count()
+
+gseg = reader.segment_count(block_index=0)
+channelspecs = reader.header["signal_channels"].dtype.names
+
+# bl = reader.read_block(lazy= False)
+seg = reader.read_segment(block_index=0, seg_index=1)
+print(seg.analogsignals)
+
+
+
+protocol = reader.read_protocol()
+rawprotocol = reader.read_raw_protocol()
+print('numberblocks: ', nblocks)
+print('samplerate: ', si)
+print('timestart: ', tstart)
+print('timestop: ', tstop)
+print('haeder: ', header)
+print('channels: ', nchannels)
+print('protocol: ', protocol)
+print('rawprotocol: ', protocol)
+print('numsegments: ', gseg)
+print('channelspecs: ', channelspecs)
